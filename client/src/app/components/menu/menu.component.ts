55ea957320d053f32f579e4406f615892452567c
Marcin-Michalik <marcin33123@gmail.com>
2024-01-03 10:04:08 +0100

    choowanie się menu przy scrolowaniu, prace nad rozsuwaniem menu dla telefonów

--- client/src/app/components/menu/menu.component.ts
+++ client/src/app/components/menu/menu.component.ts
@@ -2,6 +2,8 @@ import { Component, HostListener } from '@angular/core';
 import {LogoComponent} from "../logo/logo.component";
 import {SliderComponent} from "../slider/slider.component";
 import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 @Component({
   selector: 'app-menu',
   standalone: true,
@@ -10,17 +12,51 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     ],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
+
 })
+
 export class MenuComponent {
   isMenuOpen = false;
+  showPhoneMenu = true;
+  menuState = 'show';
 
+
+  // rozwijane menu mobilne
   @HostListener('window:resize', ['$event'])
   onResize(event: Event): void {
-    // Ustaw isMenuOpen na false, gdy szerokość okna przekroczy 768 pikseli
+    this.showPhoneMenu = window.innerWidth <= 768 ? this.showPhoneMenu : false;
     this.isMenuOpen = window.innerWidth <= 768 ? this.isMenuOpen : false;
+    this.showPhoneMenu = window.innerWidth > 768 ? this.showPhoneMenu : true;
   }
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    const menuBox = document.querySelector('.phone-menu-box');
+
+    if (menuBox) {
+      if (this.isMenuOpen) {
+        menuBox.classList.add('full');
+      } else {
+        menuBox.classList.remove('full');
+      }
+    }
   }
+
+  // scrolowanie
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const scrollPosition = window.pageYOffset;
+
+    if (scrollPosition > this.lastScrollPosition && scrollPosition > 10) {
+      // Przewijanie w dół
+      this.menuState = 'hide';
+    } else {
+      // Przewijanie do góry
+      this.menuState = 'show';
+    }
+
+    this.lastScrollPosition = scrollPosition;
+  }
+
+  private lastScrollPosition = 0;
 }
